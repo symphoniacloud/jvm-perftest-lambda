bcb83c0efdc70677d5b6dbf7f29dfffd11769240
John Chapin <john.chapin@gmail.com>
2017-06-08 14:56:44 -0400

    Update to use SAM template instead of Maven plugin for deploy

--- src/main/java/io/symphonia/CloudwatchEvent.java
+++ src/main/java/io/symphonia/CloudwatchEvent.java
@@ -1,73 +1,8 @@
 package io.symphonia;
 
-import java.util.Date;
-import java.util.Map;
-
 public class CloudwatchEvent {
 
-    private String account;
-
-    private String region;
-
-    private Map<String, String> details;
-
-    private String detailType;
-
-    private String source;
-
-    private Date time;
-
     private String id;
-
-    private String[] resources;
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    public Map<String, String> getDetails() {
-        return details;
-    }
-
-    public void setDetails(Map<String, String> details) {
-        this.details = details;
-    }
-
-    public String getDetailType() {
-        return detailType;
-    }
-
-    public void setDetailType(String detailType) {
-        this.detailType = detailType;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    public Date getTime() {
-        return time;
-    }
-
-    public void setTime(Date time) {
-        this.time = time;
-    }
 
     public String getId() {
         return id;
@@ -77,11 +12,4 @@ public class CloudwatchEvent {
         this.id = id;
     }
 
-    public String[] getResources() {
-        return resources;
-    }
-
-    public void setResources(String[] resources) {
-        this.resources = resources;
-    }
 }
